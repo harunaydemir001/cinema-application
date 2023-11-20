98fa2b63db3934ba3bda84a199e87d113d848968
Harun Aydemir <haydemir@innova.com.tr>
2023-11-20 14:35:22 +0300

    update

--- gateway-server/src/main/java/com/harun/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/harun/gatewayserver/GatewayServerApplication.java
@@ -23,17 +23,17 @@ public class GatewayServerApplication {
                 .route(p -> p
                         .path("/cinema/movie-service/***")
                         .filters(f -> f.rewritePath("/cinema/movie-service/(?<segment>.*)", RouterConstant.SEGMENT)
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader(RouterConstant.X_RESPONSE_TIME, LocalDateTime.now().toString()))
                         .uri("lb://MOVIE-SERVICE"))
                 .route(p -> p
                         .path("/cinema/director-service/***")
                         .filters(f -> f.rewritePath("/cinema/director-service/(?<segment>.*)", RouterConstant.SEGMENT)
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader(RouterConstant.X_RESPONSE_TIME, LocalDateTime.now().toString()))
                         .uri("lb://DIRECTOR-SERVICE"))
                 .route(p -> p
                         .path("/cinema/actor-service/***")
                         .filters(f -> f.rewritePath("/cinema/actor-service/(?<segment>.*)", RouterConstant.SEGMENT)
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader(RouterConstant.X_RESPONSE_TIME, LocalDateTime.now().toString()))
                         .uri("lb://ACTOR-SERVICE"))
                 .build();
     }
--- gateway-server/src/main/java/com/harun/gatewayserver/constant/RouterConstant.java
+++ gateway-server/src/main/java/com/harun/gatewayserver/constant/RouterConstant.java
@@ -2,5 +2,6 @@ package com.harun.gatewayserver.constant;
 
 public class RouterConstant {
     public static final String SEGMENT = "/${segment}";
+    public static final String X_RESPONSE_TIME= "X-Response-Time";
 
 }
